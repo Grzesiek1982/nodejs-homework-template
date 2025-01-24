b04d06ff40f173bff4a0e9e9d004d3a49dd7eafe
Grzesiek1982 <grzesieg1982@interia.pl>
2025-01-24 18:31:57 +0100

    startowe - zadanie 04

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("./passport");
+require("dotenv").config();
 
-const contactsRouter = require("./routes/api/contacts");
+const apiRouter = require("./routes/api/index");
 
 const app = express();
 
@@ -11,14 +13,13 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use("/api", apiRouter);
 
-app.use("/api/contacts", contactsRouter);
-
-app.use((req, res) => {
+app.use((_, res, __) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, _, res, __) => {
   res.status(500).json({ message: err.message });
 });
 
