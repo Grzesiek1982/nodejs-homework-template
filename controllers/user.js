110696b85a823e98773321704102ffffa7129491
Grzesiek1982 <grzesieg1982@interia.pl>
2025-02-03 17:27:23 +0100

    Dodanie brakujących funkcji do pliku user.js w controllers

--- controllers/user.js
+++ controllers/user.js
@@ -293,6 +293,155 @@ async function cleanTmpFolder() {
   }
 }
 
+// Login user
+const login = async (req, res, next) => {
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      status: "400 Bad Request",
+      contentType: "application/json",
+      responseBody: error.message,
+    });
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(401).json({
+      status: "401 Unauthorized",
+      responseBody: {
+        message: "User with this email doesn't exist",
+      },
+    });
+  }
+
+  const isPasswordValid = await user.validatePassword(req.body.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      status: "401 Unauthorized",
+      responseBody: {
+        message: "Incorrect password",
+      },
+    });
+  }
+
+  try {
+    const payload = {
+      id: user._id,
+      username: user.username,
+    };
+    const secret = process.env.AUTH_SECRET;
+    const token = jwt.sign(payload, secret, { expiresIn: "12h" });
+
+    user.token = token;
+    await user.save();
+
+    return res.json({
+      status: "200 OK",
+      contentType: "application/json",
+      responseBody: {
+        token: token,
+        user: {
+          email: req.body.email,
+          subscription: user.subscription,
+        },
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Logout user
+const logout = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    user.token = null;
+    await user.save();
+
+    return res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get current user data
+const current = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    if (!user || !user.token) {
+      return res.status(401).json({
+        status: "401 Unauthorized",
+        contentType: "application/json",
+        responseBody: {
+          message: "Not authorized",
+        },
+      });
+    }
+
+    res.json({
+      status: "200 OK",
+      contentType: "application/json",
+      responseBody: {
+        email: req.user.email,
+        subscription: req.user.subscription,
+        avatarURL: req.user.avatarURL, // Include avatar URL in response
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateSub = async (req, res, next) => {
+  const userId = req.user._id;
+  const { error } = req.body;
+
+  if (error || !req.body.subscription) {
+    return res.status(400).json({
+      status: "400 Bad Request",
+      contentType: "application/json",
+      responseBody: {
+        message: "Invalid subscription type",
+      },
+    });
+  }
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(401).json({
+        status: "401 Unauthorized",
+        contentType: "application/json",
+        responseBody: {
+          message: "Not authorized",
+        },
+      });
+    }
+
+    user.subscription = req.body.subscription;
+    await user.save();
+
+    res.json({
+      status: "200 OK",
+      contentType: "application/json",
+      responseBody: {
+        email: user.email,
+        subscription: user.subscription,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   register,
   login,
